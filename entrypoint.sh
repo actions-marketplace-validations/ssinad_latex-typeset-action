8ddda7d12c37b2a85a6b3f1e950210ffc2c01679
ssinad <27953063+ssinad@users.noreply.github.com>
2020-11-07 01:28:05 -0700

    Add symlink

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-alias python="/usr/bin/python3"
+# alias python="/usr/bin/python3"
+ln -s /usr/bin/python3 /usr/bin/python
 texliveonfly -cd -pdf -silent -usepretex='\providecommand{\finalOption}{final}' $1.tex
