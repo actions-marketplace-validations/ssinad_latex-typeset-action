59326ed5f23a9b69ebfdafb6fe124d86d861e131
ssinad <27953063+ssinad@users.noreply.github.com>
2020-11-07 01:14:58 -0700

    Update path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-texliveonfly -cd -pdf -silent -usepretex='\providecommand{\finalOption}{final}' $1.tex
+python3 -m texliveonfly -cd -pdf -silent -usepretex='\providecommand{\finalOption}{final}' $1.tex
