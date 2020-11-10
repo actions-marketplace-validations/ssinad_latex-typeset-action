bc4b282941dab3099164ac3c9e0a184b05e99bf7
ssinad <27953063+ssinad@users.noreply.github.com>
2020-11-10 00:57:31 -0700

    Rename input

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 # alias python="/usr/bin/python3"
 ln -s /usr/bin/python3 /usr/bin/python
-texliveonfly -c latexmk --terminal_only -a "-cd -pdf -usepretex='\providecommand{\finalOption}{final}'" $1.tex
+texliveonfly -c latexmk --terminal_only -a "-cd -pdf -usepretex='\providecommand{\finalOption}{final}'" $1
