152d2ebe62ced8deb69223349cf7cc05a8296abe
ssinad <27953063+ssinad@users.noreply.github.com>
2020-11-28 11:34:09 -0700

    Fix symbolic link

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # alias python="/usr/bin/python3"
-ln -s /usr/bin/python3 /usr/bin/python
+ln -s /usr/bin/python3.9 /usr/bin/python
 texliveonfly -c latexmk --terminal_only -a "-cd -pdf -usepretex='\providecommand{\finalOption}{final}'" $1
