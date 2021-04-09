bcd3a71cae1c5f0a9cfb12ad404d9616f706cf9d
ssinad <27953063+ssinad@users.noreply.github.com>
2021-04-09 12:29:40 -0600

    Revert to pdflatex

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 # alias python="/usr/bin/python3"
 # ln -s /usr/bin/python3.8 /usr/bin/python
-texliveonfly -c latexmk --terminal_only -a "-cd -lualatex -usepretex='\providecommand{\finalOption}{final}'" "$1"
+texliveonfly -c latexmk --terminal_only -a "-cd -pdf -usepretex='\providecommand{\finalOption}{final}'" "$1"
