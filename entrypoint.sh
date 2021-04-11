7244c88d949eb76f7796823f6681ceddac755473
ssinad <27953063+ssinad@users.noreply.github.com>
2021-04-11 07:13:53 -0600

    Debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 # alias python="/usr/bin/python3"
 # ln -s /usr/bin/python3.8 /usr/bin/python
-texliveonfly -c latexmk --terminal_only -a "-e '$max_repeat=2' -cd -lualatex -usepretex='\providecommand{\finalOption}{final}'" "$1"
+texliveonfly -c latexmk --terminal_only -a "-e '$max_repeat=20' -cd -lualatex -usepretex='\providecommand{\finalOption}{final}'" "$1"
