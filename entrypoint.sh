17259e09df36817b28e85e1db878da0fe08c15b7
ssinad <27953063+ssinad@users.noreply.github.com>
2021-04-13 00:16:16 -0600

    Fix bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 HOME=/root
-latexmk -cd -lualatex -interaction=nonstopmode -usepretex='\providecommand{\finalOption}{final}' "$1"
-# texliveonfly -c latexmk --terminal_only -a " -cd -lualatex -interaction=nonstopmode -usepretex='\providecommand{\finalOption}{final}'" "$1"
+texliveonfly -c latexmk --terminal_only -a " -cd -lualatex -interaction=nonstopmode -usepretex='\providecommand{\finalOption}{final}'" "$1"
